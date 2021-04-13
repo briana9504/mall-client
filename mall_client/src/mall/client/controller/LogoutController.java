ed03b4ab07f1c051fdb466f65d5de137630b39f9
GDJ34 <GDJ34@DESKTOP-MIS4LS7>
2021-04-13 14:22:44 +0900

    메인메뉴, 로그인, 로그아웃 추가

---
+++ mall_client/src/mall/client/controller/LogoutController.java
@@ -0,0 +1,23 @@
+package mall.client.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+
+/**
+ * Servlet implementation class LogoutController
+ */
+@WebServlet("/LogoutController")
+public class LogoutController extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		response.sendRedirect(request.getContextPath()+"/IndexController");
+		
+	}
+
+}
